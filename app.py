894ba1c20b76f99b77ede02319399e7bfa948215
Maesa <supathida2549@gmaul.com>
2026-09-22 15:34:02 +0700

    fix streamlit compatibility

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if uploaded_file is not None:
     try:
         # Load the image
         img = Image.open(uploaded_file)
-        st.image(img, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption="Uploaded Image", width="stretch")
         st.write("")
         st.write("Classifying...")
 
